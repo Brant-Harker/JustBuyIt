1f8fa21683a69e8e85da6d9722d23c64c9bf10e8
Sameer Ojha <sameerojha01@gmail.com>
2024-11-09 13:42:54 -0700

    Add DarkModeToggle component to the Header

---
+++ application/src/components/DarkModeToggle.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+
+export default function DarkModeToggle() {
+  const [clicked, setClicked] = useState();
+
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      setClicked(true);
+    } else {
+      setClicked(false);
+    }
+  };
+
+  return (
+    <FormGroup>
+      <FormControlLabel
+        control={<Switch checked={clicked} onChange={handleChange} />}
+        label={clicked ? "Dark Mode" : "Light Mode"}
+      />
+    </FormGroup>
+  );
+}
