fe72fe0e2469dfeb8aebc977623b8c945d71fbfe
Sameer Ojha <sameerojha01@gmail.com>
2024-11-09 12:42:24 -0700

    Move puzzles to the Puzzles component

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,36 +1,12 @@
-import { useState } from "react";
-import { Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import Header from "./components/Header";
-import Puzzle1 from "./components/Puzzle1";
-import Puzzle2 from "./components/Puzzle2";
+import Puzzles from "./components/Puzzles";
 
 export default function App() {
-  const [components, setComponents] = useState([Puzzle1, Puzzle2]);
-  const [puzzleIndex, setPuzzleIndex] = useState(0);
-
-  const updateIndex = () => {
-    if (puzzleIndex < components.length - 1) {
-      setPuzzleIndex(puzzleIndex + 1);
-    } else {
-      setPuzzleIndex(0);
-    }
-  };
-
-  setInterval(() => {
-    updateIndex();
-  }, 10000);
-
   return (
     <Container maxWidth="md">
       <Header />
-      {components.map(
-        (Component, key) =>
-          key == puzzleIndex && (
-            <span key={key}>
-              <Component key={key} />
-            </span>
-          )
-      )}
+      <Puzzles />
     </Container>
   );
 }
--- application/src/components/Puzzle.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-const Puzzle = ({ puzzles }) => {
-  const [puzzleNum, setPuzzleNum] = useState();
-
-  return <div>Puzzle</div>;
-};
-
-export default Puzzle;
