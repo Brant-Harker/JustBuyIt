f301f4eb7b9da5a91a1612460fd53306dd6a2904
Brant-Harker <brant.harker12@gmail.com>
2024-11-09 11:45:28 -0700

    Basic scroll template

--- application/src/ScrollMaze/ScrollMaze.jsx
+++ application/src/ScrollMaze/ScrollMaze.jsx
@@ -1,24 +1,32 @@
-import { Box } from '@mui/material';
-import Grid from '@mui/material/Grid2'
+import './ScrollMaze.css'
 
 function ScrollMaze() {
 
   return (
-    <Box>
-      <Grid container columns={50} style={{width: 10000, height: 500 }}>
-        <Grid size={47} style={{backgroundColor: 'red'}}>
-          <p>Welcome to Hell!</p>
-        </Grid>
-        <Grid size={1} style={{ maxHeight: 500, overflow: 'auto', backgroundColor: 'green'}}>
-          <p style={{marginBottom: 1000}}>Getting warmer!</p>
-          <Grid container style={{backgroundColor: 'purple'}}>
-            <Grid size={12} style={{width: 10000, overflow: 'auto',}}>
-              <p>asshole</p>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Box>
+    <div className='maze'>
+      
+      <div className='container container-row'>
+
+        <div className='spacer-right'/>
+
+        <div className='container container-column'>
+          <div className='spacer-bottom'/>
+
+          <div className='container container-row'>
+            <div className='spacer-right'></div>
+          </div>
+
+          <div className='spacer-bottom'/>
+
+          <div className='container container-row'>
+            <div className='spacer-right'/>
+          </div>
+          
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
