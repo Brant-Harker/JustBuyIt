c0997f9691ee56b18b787ca33b3834197dbfb5c6
Sameer Ojha <sameerojha01@gmail.com>
2024-11-09 19:37:52 -0700

    Add additional text to the CancelScreen

--- application/src/components/CancelScreen.jsx
+++ application/src/components/CancelScreen.jsx
@@ -1,11 +1,33 @@
-import { Button, Card, CardActions, CardContent } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Input,
+  InputLabel,
+  Typography,
+  FormControl,
+} from "@mui/material";
 
 const CancelScreen = () => {
   return (
     <Card variant="outlined">
-      <CardContent></CardContent>
+      <CardContent>
+        <Typography
+          sx={{
+            color: "red",
+          }}
+        >
+          Cancel Order
+        </Typography>
+        <Typography>Ok fine... we get it.</Typography>
+        <FormControl>
+          <InputLabel htmlFor="message"></InputLabel>
+          <Input id="message" />
+        </FormControl>
+      </CardContent>
       <CardActions>
-        <Button color="danger">Cancel Order</Button>
+        <Button color="danger">Yes, cancel it</Button>
       </CardActions>
     </Card>
   );
