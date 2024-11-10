d2a93b914ff3e7901faaebd7a90a861c3cfd0010
Mihiri Kamiss <76460057+Jaymerist@users.noreply.github.com>
2024-11-10 11:30:59 -0700

    Revert "Styled this bad boy"
    This reverts commit 356a9d7816f9b902d0655d3451a3d1c41cf61b64.

--- chrm-2/src/scripts/popup.js
+++ chrm-2/src/scripts/popup.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Automatically redirect to process.html after 10 seconds
     setTimeout(() => {
         window.location.href = 'process.html';
-    }, 20000);
+    }, 10000);
 });
 
 // Fetch and store the price locally when popup opens
@@ -39,54 +39,4 @@ getPriceFromBackground()
     })
     .catch(error => {
         console.error('Error getting price:', error);
-    });
-
-//get product name
-
-async function getProductNameFromBackground() {
-        return new Promise((resolve, reject) => {
-            chrome.runtime.sendMessage({ action: 'getProductName' }, (response) => {
-                if (chrome.runtime.lastError) {
-                    reject('Error receiving message: ' + chrome.runtime.lastError.message);
-                } else if (response && response.price) {
-                    resolve(response.price);
-                } else {
-                    reject('Product name not found');
-                }
-            });
-        });
-    }
-    
-    getProductNameFromBackground()
-        .then(productName => {
-            console.log('Product name received:', productName);
-            chrome.storage.local.set({ productName });
-        })
-        .catch(error => {
-            console.error('Error getting productName:', error);
-        });
-
-//get sale amount
-
-async function getSaleAmountFromBackground() {
-    return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage({ action: 'getSaleAmount' }, (response) => {
-            if (chrome.runtime.lastError) {
-                reject('Error receiving message: ' + chrome.runtime.lastError.message);
-            } else if (response && response.price) {
-                resolve(response.price);
-            } else {
-                reject('Sale amount not found');
-            }
-        });
-    });
-}
-
-getSaleAmountFromBackground()
-    .then(saleAmount => {
-        console.log('Sale amount received:', saleAmount);
-        chrome.storage.local.set({ saleAmount });
-    })
-    .catch(error => {
-        console.error('Error getting saleAmount:', error);
     });
