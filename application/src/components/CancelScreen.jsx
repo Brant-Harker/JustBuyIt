cd41b9b46976277c23c276bae6169d8c3d1ffbfa
walter-vasquez <vasquez.wa@gmail.com>
2024-11-10 11:39:47 -0700

    Add styling to card component

--- application/src/components/CancelScreen.jsx
+++ application/src/components/CancelScreen.jsx
@@ -7,30 +7,44 @@ import {
   InputLabel,
   Typography,
   FormControl,
-} from "@mui/material";
+} from "@mui/material"
 
 const CancelScreen = () => {
   return (
-    <Card variant="outlined">
-      <CardContent>
-        <Typography
-          sx={{
-            color: "red",
-          }}
-        >
-          Cancel Order
-        </Typography>
-        <Typography>Ok fine... we get it.</Typography>
-        <FormControl>
-          <InputLabel htmlFor="message"></InputLabel>
-          <Input id="message" />
-        </FormControl>
-      </CardContent>
-      <CardActions>
-        <Button color="danger">Yes, cancel it</Button>
-      </CardActions>
-    </Card>
-  );
-};
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card variant="outlined" sx={{ maxWidth: 400, padding: 2 }}>
+        <CardContent>
+          <Typography
+            sx={{
+              color: "red",
+              fontSize: 24,
+              fontWeight: "bold",
+              mb: 1,
+            }}
+          >
+            Cancel Order
+          </Typography>
+          <Typography sx={{ mb: 2 }}>Ok fine... we get it.</Typography>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="message">Message</InputLabel>
+            <Input id="message" />
+          </FormControl>
+        </CardContent>
+        <CardActions>
+          <Button color="error" variant="contained" fullWidth>
+            Yes, cancel it
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
+}
 
-export default CancelScreen;
+export default CancelScreen
