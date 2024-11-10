b5c51eb6ee9785899163d67f7ba356e36e628ec1
Sameer Ojha <sameerojha01@gmail.com>
2024-11-09 18:44:47 -0700

    Add styles to app and fix cursor following effect

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,55 +1,84 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import Grid from "@mui/material/Grid2";
 import { Box, Button } from "@mui/material";
 import Header from "./components/Header";
 import Puzzles from "./components/Puzzles";
 import "./index.css";
+import heroImage from "./assets/hero1.png";
 
 export default function App() {
-  const [leftPos, setLeftPos] = useState("calc(50% - 2rem)");
-  const [topPos, setTopPos] = useState("calc(50% - 2rem)");
+  const [leftPos, setLeftPos] = useState("calc(50% - 3rem)");
+  const [topPos, setTopPos] = useState("calc(50% - 3rem)");
   const [displayPuzzles, setDisplayPuzzles] = useState(true);
   const [isDarkMode, setIsDarkMode] = useState(false);
   const flashlight = useRef(null);
 
-  const HALF_WIDTH = 32;
-  const handleMousemove = (e) => {
-    setLeftPos(e.clientX - HALF_WIDTH);
-    setTopPos(e.clientY - HALF_WIDTH);
-  };
+  const HALF_WIDTH = 48;
+  useEffect(() => {
+    const handleMousemove = (e) => {
+      const newLeftPos = e.clientX;
+      const newTopPos = e.clientY;
+
+      setLeftPos(newLeftPos);
+      setTopPos(newTopPos);
+    };
+
+    document.addEventListener("mousemove", handleMousemove);
+  }, []);
 
   return (
     <Grid
       sx={{
-        height: "100vh !important",
-        width: "100vw !important",
-        backgroundColor: isDarkMode && "Black",
-        position: "relative",
+        height: "100vh",
+        width: "100vw",
       }}
       container
-      onMouseMove={handleMousemove}
     >
       <Grid size={12} sx={{ minHeight: "3rem", maxHeight: "3rem" }}>
         <Header setIsDarkMode={setIsDarkMode} />
       </Grid>
-      <Grid
-        size={12}
-        sx={{ height: "calc(100vh - 3rem)", width: "100vw", padding: "1rem" }}
-      >
+      <Grid size={12}>
+        {isDarkMode && (
+          <Box
+            sx={{
+              height: "calc(100vh - 3rem)",
+              width: "100vw",
+              padding: "1rem",
+              backgroundImage: `linear-gradient(#000, #000), url(${heroImage})`,
+              backgroundRepeat: isDarkMode && "no-repeat",
+              backgroundSize: isDarkMode && "100% 100%",
+              backgroundAttachment: isDarkMode && "fixed",
+              backgroundPosition: isDarkMode && "center",
+              position: "relative",
+              cursor: "none",
+            }}
+          >
+            <Box
+              id="flashlight"
+              ref={flashlight}
+              sx={{
+                left: `${leftPos}px`,
+                top: `${topPos}px`,
+                position: "absolute",
+                width: "6rem",
+                height: "6rem",
+                zIndex: 1,
+                backgroundImage: `url(${heroImage})`,
+                backgroundSize: "100% 100%",
+                backgroundPosition: "center",
+                backgroundAttachment: "fixed",
+                borderRadius: "50%",
+                boxShadow: "0 0 5rem 1rem rgb(215, 195, 9)",
+              }}
+            />
+          </Box>
+        )}
         {displayPuzzles && <Puzzles />}
       </Grid>
-      <Box
-        id="flashlight"
-        ref={flashlight}
-        sx={{
-          left: `${leftPos}px`,
-          top: `${topPos}px`,
-        }}
-      ></Box>
       <Button
         variant="contained"
         sx={{
-          position: "fixed",
+          position: "absolute",
           right: "3rem",
           bottom: "3rem",
         }}
